fca32fb84ca9865571ad358a7d43bf8ba23d6bd9
KevinLmK0702 <KevinLeeAa070212@outlook.com>
2026-09-19 08:32:11 +0800

    mod: ReSukiSU, SUSFS: merge SUSFS latest commits and sync ReSukiSU changes (20260919)

--- fs/namei.c
+++ fs/namei.c
@@ -1711,6 +1711,7 @@ retry:
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 	dentry = lookup_real(base->d_inode, dentry, flags);
+	bool is_fuse = dentry->d_inode->i_sb->s_magic == FUSE_SUPER_MAGIC;
 
 	if (unlikely(dentry) && !IS_ERR(dentry) && dentry->d_inode && !found_sus_path && susfs_is_inode_sus_path(dentry->d_inode)) {
 		if (d_in_lookup(dentry))
@@ -1721,7 +1722,7 @@ retry:
 		//   it is trying to find the fuse sus path with the create flag, then
 		//   at least we can prevent the fake qstr file from from being created,
 		//   although it is futile to do this, it is better than doing nothing.
-		if (dentry->d_inode->i_sb->s_magic == FUSE_SUPER_MAGIC &&
+		if (is_fuse &&
 			(flags & (LOOKUP_CREATE | LOOKUP_EXCL)))
 			return ERR_PTR(-EACCES);
 		dentry = d_alloc(base, &susfs_fake_qstr_name);
@@ -3800,14 +3801,14 @@ static int do_tmpfile(struct nameidata *nd, unsigned flags,
 	{
 		fake_filename = susfs_open_redirect_spoof_do_sys_openat(path.dentry->d_inode);
 		if (fake_filename && !IS_ERR(fake_filename)) {
+			struct path fake_path;
+
+			/* filename_lookup() consumes fake_filename */
+			error = filename_lookup(old_dfd, fake_filename, flags | LOOKUP_DIRECTORY, &fake_path, NULL);
 			path_put(&path);
-			restore_nameidata();
-			set_nameidata(nd, old_dfd, fake_filename);
-			error = path_lookupat(nd, flags | LOOKUP_DIRECTORY, &path);
-			if (unlikely(error)) {
-				putname(fake_filename);
+			if (unlikely(error))
 				return error;
-			}
+			path = fake_path;
 		}
 	}
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
@@ -3838,10 +3839,6 @@ out2:
 	mnt_drop_write(path.mnt);
 out:
 	path_put(&path);
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	if (fake_filename && !IS_ERR(fake_filename))
-		putname(fake_filename);
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 	return error;
 }
 
@@ -3860,14 +3857,14 @@ static int do_o_path(struct nameidata *nd, unsigned flags, struct file *file)
 		{
 			fake_filename = susfs_open_redirect_spoof_do_sys_openat(path.dentry->d_inode);
 			if (fake_filename && !IS_ERR(fake_filename)) {
+				struct path fake_path;
+
+				/* filename_lookup() consumes fake_filename */
+				error = filename_lookup(old_dfd, fake_filename, flags, &fake_path, NULL);
 				path_put(&path);
-				restore_nameidata();
-				set_nameidata(nd, old_dfd, fake_filename);
-				error = path_lookupat(nd, flags, &path);
-				if (unlikely(error)) {
-					putname(fake_filename);
+				if (unlikely(error))
 					return error;
-				}
+				path = fake_path;
 			}
 		}
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
@@ -3875,10 +3872,6 @@ static int do_o_path(struct nameidata *nd, unsigned flags, struct file *file)
 		error = vfs_open(&path, file);
 		path_put(&path);
 	}
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	if (fake_filename && !IS_ERR(fake_filename))
-		putname(fake_filename);
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 	return error;
 }
 
@@ -3888,6 +3881,7 @@ static struct file *path_openat(struct nameidata *nd,
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 	int old_dfd = nd->dfd;
 	struct filename *fake_filename = NULL;
+	struct filename *old_name = nd->name;
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 	const char *s;
 	struct file *file;
@@ -3960,8 +3954,10 @@ out2:
 		put_filp(file);
 	}
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	if (fake_filename && !IS_ERR(fake_filename))
+	if (fake_filename && !IS_ERR(fake_filename)) {
+		nd->name = old_name;
 		putname(fake_filename);
+	}
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 	if (unlikely(error)) {
 		if (error == -EOPENSTALE) {
